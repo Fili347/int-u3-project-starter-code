2c0953a4920507ee064dcc64a9fc8491c2bdf969
Fili347 <filitekle228@gmail.com>
2025-06-13 00:52:20 -0400

    adventure.game

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Declare variables below to save the different divs of your story.
 
+let storyOpeningScreen = document.querySelector(".story-opening");
+let optionOneButton = document.querySelector(".option-one");
+let optionTwoButton = document.querySelector(".option-two");
 
-
+let optionOneScreen = document.querySelector( ".option-one-screen");
+let optionTwoScreen = document.querySelector( ".option-two-screen");
 
 
 
@@ -12,13 +16,20 @@
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionOneButton.addEventListener('click', function(){
+  optionOneScreen.style.display = "block";
+  optionTwoScreen.style.display = "none";
+  storyOpeningScreen.style.display = "none";
+  optionOneButton.style.display = "none";
+  optionTwoButton.style.display = "none";
+});
 
-// });
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+optionTwoButton.addEventListener('click', function(){
+  optionOneScreen.style.display = "none";
+  optionTwoScreen.style.display = "block";
+  optionTwoButton.style.display = "none";
+  optionOneButton.style.display = "none";
+});
 
 
 // INSERT_VARIABLE.addEventListener('click', function(){
